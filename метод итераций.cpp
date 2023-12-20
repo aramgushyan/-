52832e7699679223496116444be90ec83025a508
Aram Gushyan <149182384+aramgushyan@users.noreply.github.com>
2023-12-20 09:13:54 +0500

    Add files via upload

---
+++ метод итераций.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+const double e = 0.000001;
+double a,b,c;
+double u(double x)
+{
+	return cos(pow(1 - 0.3 * pow(x, 3), 0.5));
+}
+double u1(double x)
+{
+	return sin(pow(1 - 0.3 * pow(x, 3), 0.5)) * 0.9 * pow(x, 2);
+}
+
+int main()
+{
+	a = 0;
+	b = 1;
+	c = (a + b) / 2;
+	if (fabs(u1(c)) < 1)
+	{
+		while (fabs(c - a) > e)
+		{
+			a = c;
+			c = u(c);
+		}
+		cout << c;
+	}
+	return 0;
+
+}
---
+++ метод половинного деления.cpp
@@ -0,0 +1,23 @@
+﻿#include <cmath>
+#include <iostream>
+using namespace std;	
+double a, b, c;
+const double e = 0.00001;
+double f(double x)
+{
+	return acos(x) - pow((1 - 0.3 * pow(x, 3)), 0.5);
+}
+int main()
+{
+	a = 0;
+	b = 1;
+	while (b - a > e) {
+		c = (a + b) / 2;
+		if ((f(b) * f(c)) < 0)
+			a = c;
+		else
+		b = c;
+	}
+	cout << (a + b) / 2 << endl;
+	return 0;
+}
